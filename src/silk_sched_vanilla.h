cd7c61f154bd80681c0338ca670023811cd23efb
Eitan Ben-Amos <eitan_ben_amos@hotmail.com>
2012-11-28 02:50:10 -0500

    vanilla scheduler queue is now mutex protected for correctness (we dont care about its performance anyway).

--- src/silk_sched_vanilla.h
+++ src/silk_sched_vanilla.h
@@ -11,10 +11,19 @@
  * 2) queue access requires a lock
  * 3) queue is processed with strict order of FIFO
  * 4) when terminated, will process the whole queue until it pops the SILK_MSG_TERM msg
+ *
+ * Notes:
+ * Some API's have both a locked & unlocked version. the unlocked has a 
+ * preceding "_" but is otherwise identical
+ *
+ * 
+ * TODO: the whole scheduler is here !!!
+ * it should move into its own C file & some code should be generic for ANY scheduler.
  */
 
-
 struct silk_incoming_msg_queue_t {
+    // a mutex to guard any access to the queue
+    pthread_mutex_t              mtx;
     // a fixed number of msgs in a simple msg queue.
 #define MSG_QUEUE_SIZE           8*1024 // msgs
     struct silk_msg_t            msgs[MSG_QUEUE_SIZE];
@@ -30,6 +39,7 @@ silk_sched_init(struct silk_incoming_msg_queue_t      *q)
     q->next_write = 0;
     q->next_read = 0;
     memset(q->msgs, 0, sizeof(q->msgs));
+    pthread_mutex_init(&q->mtx, NULL);
     return SILK_STAT_OK;
 }
 
@@ -39,11 +49,13 @@ silk_sched_init(struct silk_incoming_msg_queue_t      *q)
 enum silk_status_e
 silk_sched_terminate(struct silk_incoming_msg_queue_t      *q)
 {
+    pthread_mutex_destroy(&q->mtx);
     return SILK_STAT_OK;
 }
 
 /*
  * move the index into the next slot
+ * BEWARE: queue must be locked
  */
 static inline uint32_t
 silk_sched_next_index(uint32_t       index)
@@ -54,8 +66,12 @@ silk_sched_next_index(uint32_t       index)
 	index = 0;
     return index;
 }
+
+/*
+ * BEWARE: queue must be locked
+ */
 static inline bool
-silk_sched_is_empty(struct silk_incoming_msg_queue_t      *q)
+_silk_sched_is_empty(struct silk_incoming_msg_queue_t      *q)
 {
     if (q->next_write == q->next_read) {
 	return true;
@@ -63,14 +79,39 @@ silk_sched_is_empty(struct silk_incoming_msg_queue_t      *q)
 	return false;
     }
 }
+
 static inline bool
-silk_sched_is_full(struct silk_incoming_msg_queue_t      *q)
+silk_sched_is_empty(struct silk_incoming_msg_queue_t      *q)
+{
+    bool   ret;
+
+    pthread_mutex_lock(&q->mtx);
+    ret = silk_sched_is_empty(q);
+    pthread_mutex_unlock(&q->mtx);
+    return ret;
+}
+/*
+ * BEWARE: queue must be locked
+ */
+static inline bool
+_silk_sched_is_full(struct silk_incoming_msg_queue_t      *q)
 {
     if (silk_sched_next_index(q->next_write) == q->next_read) {
 	return true;
     } else {
 	return false;
     }
+}
+
+static inline bool
+silk_sched_is_full(struct silk_incoming_msg_queue_t      *q)
+{
+    bool   ret;
+
+    pthread_mutex_lock(&q->mtx);
+    ret = _silk_sched_is_full(q);
+    pthread_mutex_unlock(&q->mtx);
+    return ret;
 }
 /*
  * if queue isnt full, write the msg into the tail of the queue
@@ -80,12 +121,20 @@ enum silk_status_e
 silk_sched_send(struct silk_incoming_msg_queue_t      *q,
 		struct silk_msg_t                     *msg)
 {
-    if (silk_sched_is_full(q)) {
-	return SILK_STAT_Q_FULL;
+    enum silk_status_e   silk_stat;
+
+    pthread_mutex_lock(&q->mtx);
+    if (_silk_sched_is_full(q)) {
+	silk_stat = SILK_STAT_Q_FULL;
+	goto out;
     }
     q->msgs[q->next_write] = *msg;
     q->next_write++;
-    return SILK_STAT_OK;
+    silk_stat = SILK_STAT_OK;
+
+ out:
+    pthread_mutex_unlock(&q->mtx);
+    return silk_stat;
 }
 
 /*
@@ -96,12 +145,20 @@ silk_sched_send(struct silk_incoming_msg_queue_t      *q,
 bool silk_sched_get_next(struct silk_incoming_msg_queue_t      *q,
 			 struct silk_msg_t                     *msg)
 {
-    if (silk_sched_is_empty(q)) {
-	return false;
+    bool ret;
+
+    pthread_mutex_lock(&q->mtx);
+    if (_silk_sched_is_empty(q)) {
+	ret = false;
+	goto out;
     }
     *msg = q->msgs[q->next_read];    
     q->next_read = silk_sched_next_index(q->next_read);
-    return true;
+    ret = true;
+
+ out:
+    pthread_mutex_unlock(&q->mtx);
+    return ret;
 }
 
 
